95814d505816f4d6e278a89d1a9c765ec32101e9
Kal Draganov <kaldraganov05@gmail.com>
2021-03-22 17:51:22 -0400

    Slightly increase fine-tuning scale

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -27,17 +27,20 @@ public class Drive extends CommandBase {
 
         if (m_drivetrain.isSingleStickDrive()) {
             if (Math.abs(xbox.getY(Hand.kLeft)) > 0.1 || Math.abs(-xbox.getX(Hand.kLeft)) > 0.1) {
+                // Normal single stick drive
                 m_drivetrain.getDifferentialDrive().arcadeDrive(
                     xbox.getY(Hand.kLeft),
                     -xbox.getX(Hand.kLeft));
             }
             else {
+                // Fine-tuning single stick drive
                 m_drivetrain.getDifferentialDrive().arcadeDrive(
-                    xbox.getY(Hand.kRight)/2,
-                    -xbox.getX(Hand.kRight)/2);
+                    xbox.getY(Hand.kRight) / Math.sqrt(2),
+                    -xbox.getX(Hand.kRight) / Math.sqrt(2));
             }
         }
         else {
+            // Split control drive
             m_drivetrain.getDifferentialDrive().arcadeDrive(
                 xbox.getY(Hand.kLeft),
                 -xbox.getX(Hand.kRight));
