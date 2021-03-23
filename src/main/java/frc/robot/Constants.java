70b86bc409659b12690210be2e334f7a3ec22949
Kal Draganov <kaldraganov05@gmail.com>
2021-03-22 22:10:53 -0400

    Switch to the calculated motor speeds

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,20 +10,20 @@ public class Constants {
         public static double feeder = 0.9;
 
         // Shooter motor speeds in red zone
-        public static double red_top = 0.7;
-        public static double red_bottom = 0.7;
+        public static double red_top = 1.0;
+        public static double red_bottom = 0.768;
 
         // Shooter motor speeds in blue zone
-        public static double blue_top = 0.6;
-        public static double blue_bottom = 0.6;
+        public static double blue_top = 1.0;
+        public static double blue_bottom = 0.725;
 
         // Shooter motor speeds in yellow zone
-        public static double yellow_top = 0.5;
-        public static double yellow_bottom = 0.5;
+        public static double yellow_top = 1.0;
+        public static double yellow_bottom = 0.8;
 
         // Shooter motor speeds in green zone
-        public static double green_top = 0.4;
-        public static double green_bottom = 0.4;
+        public static double green_top = 0.43;
+        public static double green_bottom = 0.43;
     }
 
 }
