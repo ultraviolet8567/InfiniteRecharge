e601845e6ce6dda68ec876b8bb4c31af92397ec0
Kal Draganov <kaldraganov05@gmail.com>
2021-03-26 17:02:04 -0400

    Update motor presets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,19 +11,19 @@ public class Constants {
 
         // Shooter motor speeds in red zone
         public static double red_top = 1.0;
-        public static double red_bottom = 0.768;
+        public static double red_bottom = 0.675;
 
         // Shooter motor speeds in blue zone
         public static double blue_top = 1.0;
-        public static double blue_bottom = 0.725;
+        public static double blue_bottom = 0.646;
 
         // Shooter motor speeds in yellow zone
         public static double yellow_top = 1.0;
-        public static double yellow_bottom = 0.8;
+        public static double yellow_bottom = 0.659;
 
         // Shooter motor speeds in green zone
-        public static double green_top = 0.43;
-        public static double green_bottom = 0.43;
+        public static double green_top = 0.4;
+        public static double green_bottom = 0.41;
     }
 
 }
